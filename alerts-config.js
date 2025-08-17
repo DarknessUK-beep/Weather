30df7d9646e38f9c9f9cc150d9a46d6867e90443
DarknessUK-beep <DarknessUK_@hotmail.com>
2025-08-17 16:13:15 +0100

    Update alerts-config.js

--- alerts-config.js
+++ alerts-config.js
@@ -6,4 +6,7 @@
 window.NSWWS = {
   API_KEY: "",        // e.g. "abC123..."
   ATOM_FEED_URL: ""   // e.g. "https://example.api.metoffice.gov.uk/nswws/atom/feed"
+  window.NSWWS = {
+  API_KEY: "YOUR_KEY_HERE",
+  ATOM_FEED_URL: "YOUR_FEED_URL_HERE"
 };
